b65768dbf9ef55d4ffe668f9722597ef71b86f8a
Noel Mier-Luna <noel.lmier@gmail.com>
2026-03-19 20:43:23 -0700

    First demo test

--- 210-midterm1.cpp
+++ 210-midterm1.cpp
@@ -39,14 +39,14 @@ public:
 
         Node *newNode = new Node(value);
         if (!head) // The list traverses and sees if head = nullptr.
-        { // If true, new dynamic node is created and becomes both head AND tail of list.
+        {          // If true, new dynamic node is created and becomes both head AND tail of list.
             head = tail = newNode;
             return; // Exits function after this is done. Position not needed here since list was empty.
         }
 
-        Node *temp = head; // If list is not empty, a temporary pointer is needed to traverse and insert node properly.
-        for (int i = 0; i < position && temp; ++i) // Loop points temp pointer to each node until it reaches 
-            temp = temp->next; // position or end of list.
+        Node *temp = head;                         // If list is not empty, a temporary pointer is needed to traverse and insert node properly.
+        for (int i = 0; i < position && temp; ++i) // Loop points temp pointer to each node until it reaches
+            temp = temp->next;                     // position or end of list.
 
         if (!temp)
         { // If desired position is greater than list size, temp will point to nullptr
@@ -54,37 +54,37 @@ public:
             delete newNode; // Deletes new node since it can't be inserted to prevent memory leak and exits function.
             return;
         }
-// Once position is found, our temp pointer is pointing to node we will insert after.
-        newNode->next = temp->next;// New node's next pointer is set to temp's next pointer to prepare for insertion.
-        newNode->prev = temp; // We do the same for new node's prev pointer, otherwise it would be left as nullptr and break the list.
-        if (temp->next) // Temp's next pointer points back to temp as prev pointer if temp->next is not nullptr. 
+        // Once position is found, our temp pointer is pointing to node we will insert after.
+        newNode->next = temp->next;     // New node's next pointer is set to temp's next pointer to prepare for insertion.
+        newNode->prev = temp;           // We do the same for new node's prev pointer, otherwise it would be left as nullptr and break the list.
+        if (temp->next)                 // Temp's next pointer points back to temp as prev pointer if temp->next is not nullptr.
             temp->next->prev = newNode; // Ensures node after temp points back to new node as prev pointer.
-        else // If temp-next = nullptr, then temp is the tail and new node becomes the new tail.
+        else                            // If temp-next = nullptr, then temp is the tail and new node becomes the new tail.
             tail = newNode;
         temp->next = newNode; // Temp's next pointer set to new node to complete insertion.
     }
 
     void delete_val(int value)
-    { // Deletes first node found with data member = value.
+    {              // Deletes first node found with data member = value.
         if (!head) // If list is empty (head = nullptr), exits function since there is nothing to delete.
             return;
 
         Node *temp = head; // Temp traversal pointer starts at head.
 
         while (temp && temp->data != value) // As long as temp != nullptr and current node's data != value,
-            temp = temp->next; // temp moves to next node. Repeats until value found or end of list reached.
+            temp = temp->next;              // temp moves to next node. Repeats until value found or end of list reached.
 
         if (!temp) // Condition described above^. Exits function if condition is true.
             return;
 
-        if (temp->prev) // If temp's prev pointer != nullptr, temp is not head of list.
+        if (temp->prev)                    // If temp's prev pointer != nullptr, temp is not head of list.
             temp->prev->next = temp->next; // Sets temp's PREV NODE's next pointer to temp's next pointer to prepare for deletion.
-        else // If temp's prev pointer = nullptr, temp IS head of list.
-            head = temp->next; // Changes head pointer to next node since current head is being deleted.
+        else                               // If temp's prev pointer = nullptr, temp IS head of list.
+            head = temp->next;             // Changes head pointer to next node since current head is being deleted.
 
-        if (temp->next) // Same logic as above, but for temp's next pointer. If temp's next pointer != nullptr,
+        if (temp->next)                    // Same logic as above, but for temp's next pointer. If temp's next pointer != nullptr,
             temp->next->prev = temp->prev; // sets temp's NEXT NODE's prev pointer to temp's prev pointer to prepare deletion.
-        else // Otherwise, changes tail pointer to temp's prev node since temp is the tail and will be deleted.
+        else                               // Otherwise, changes tail pointer to temp's prev node since temp is the tail and will be deleted.
             tail = temp->prev;
 
         delete temp; // Deletes temp node to free memory and complete deletion.
@@ -93,13 +93,13 @@ public:
     void delete_pos(int pos)
     { // Deletes node AT chosen position. This time position is indexed starting at 1.
         if (!head)
-        { //Displays message and exits function is list is empty. Nothing to delete.
+        { // Displays message and exits function is list is empty. Nothing to delete.
             cout << "List is empty." << endl;
             return;
         }
 
         if (pos == 1)
-        { // If position is 1, calls pop_front function to delete head of list.
+        {                // If position is 1, calls pop_front function to delete head of list.
             pop_front(); // Function handles pointer updates and deletion.
             return;
         }
@@ -123,11 +123,11 @@ public:
         }
 
         if (!temp->next)
-        { // If temp's next pointer = nullptr, temp is the tail and pop_back function can be called to delete it.
+        {               // If temp's next pointer = nullptr, temp is the tail and pop_back function can be called to delete it.
             pop_back(); // Also handles all pointer updates and deletion.
             return;
         }
- // If temp is not head or tail, temp's prev and next nodes need to be linked together to keep list intact after deletion.
+        // If temp is not head or tail, temp's prev and next nodes need to be linked together to keep list intact after deletion.
         Node *tempPrev = temp->prev; // Otherwise, list will be broken since temp's pointers will dangle after deletion.
         tempPrev->next = temp->next;
         temp->next->prev = tempPrev;
@@ -140,7 +140,7 @@ public:
         if (!tail) // If list is empty (tail = nullptr), new node becomes both head and tail of list.
             head = tail = newNode;
         else
-        { // If not empty, new node is added after tail and becomes new tail.
+        {                         // If not empty, new node is added after tail and becomes new tail.
             tail->next = newNode; // Old tail's next pointer is set to new node to prepare for insertion.
             newNode->prev = tail;
             tail = newNode; // Tail pointer is updated to new node to complete insertion.
@@ -210,8 +210,8 @@ public:
         }
     }
     void print()
-    { // Prints list from head to tail.
-        Node *current = head;
+    {                         // Prints list from head to tail.
+        Node *current = head; // Traversal pointer.
         if (!current)
         {
             cout << "List is empty." << endl;
@@ -240,11 +240,37 @@ public:
         }
         cout << endl;
     }
+
+    void every_other_element()
+    { // Prints every other element in list. i.e: "Head, 3rd node, 5th node"
+        Node *current = head;
+        if (!current) // Checks if list is empty.
+        {
+            cout << "List is empty." << endl;
+            return;
+        }
+        int count = 1;  // Counter to keep track of node positions.
+        while (current) // Moves through list and prints data of every other node based on count variable.
+        {
+            if (count % 2 != 0) // If count is odd, current node's data is printed.
+                cout << current->data << " ";
+            current = current->next; // Moves to next node and increments count until end of list reached.
+            count++;
+        }
+    }
 };
 
-int main()
-{
-    cout << MIN_NR + MIN_LS + MAX_NR + MAX_LS; // dummy statement to avoid compiler warning
+    int main()
+    {
+        cout << "Demo for every_other_element function:" << endl;
+        DoublyLinkedList list;
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+        list.push_back(4);
+        list.push_back(5);
+        cout << "List: ";
+        list.every_other_element(); // Should print 1, 3, and 5 since those are the odd indexed nodes in the list.
 
-    return 0;
-}
+        return 0;
+    }
